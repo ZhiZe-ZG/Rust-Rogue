f3b999ad2151cff0379ad89642e355ab307a010d
Qi Wang <qi_wang_zz@outlook.com>
2026-08-25 13:26:21 +0800

    Refactor: Update nixpkgs reference to nixos-26.05 and remove unused darwin system

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nix flake for Rogue 5.4.4";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs =
@@ -11,7 +11,6 @@
       systems = [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
         "aarch64-darwin"
       ];
 
@@ -25,55 +24,6 @@
         );
     in
     {
-      packages = forAllSystems (
-        { pkgs }:
-        let
-          rogue = pkgs.stdenv.mkDerivation {
-            pname = "rogue";
-            version = "5.4.4";
-            src = self;
-
-            nativeBuildInputs = [
-              pkgs.pkg-config
-              pkgs.cargo
-              pkgs.rustc
-            ];
-
-            buildInputs = [
-              pkgs.ncurses
-            ];
-
-            configureFlags = [
-              "--with-program-name=rogue"
-            ];
-
-            installPhase = ''
-              runHook preInstall
-
-              install -Dm755 rogue $out/bin/rogue
-
-              if [ -f rogue.6 ]; then
-                install -Dm644 rogue.6 $out/share/man/man6/rogue.6
-              fi
-
-              runHook postInstall
-            '';
-
-            meta = with pkgs.lib; {
-              description = "Classic Rogue dungeon crawler";
-              homepage = "https://github.com/rogueforge/rogue";
-              license = licenses.bsd3;
-              platforms = platforms.unix;
-              mainProgram = "rogue";
-            };
-          };
-        in
-        {
-          default = rogue;
-          rogue = rogue;
-        }
-      );
-
       devShells = forAllSystems (
         { pkgs }: {
           default = pkgs.mkShell {
